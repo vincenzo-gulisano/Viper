f32e600075736d0f0e269f454eddfc3c3438335c
Vincenzo Massimiliano Gulisano <vincenzo.gulisano@chalmers.se>
2015-09-09 14:09:31 +0200

    completing python script

--- python/experiments_automation/find_best_configuration.py
+++ python/experiments_automation/find_best_configuration.py
@@ -1,10 +1,49 @@
 import analyze_topology_results
+import os
+import time
+
+
+def run_exp(stats_folder, jar, main, id_prefix, duration, repetitions, operators, instances):
+    for r in range(0, repetitions):
+
+        exp_id = str(r) + '_'
+        for o in operators:
+            exp_id += str(instances[o]) + '_'
+        exp_id += id_prefix
+
+        command = '/home/vincenzo/storm/apache-storm-0.9.5/bin/storm jar ' + jar + ' ' + main + ' false true ' + \
+                  stats_folder + ' ' + exp_id + ' ' + str(duration) + ' '
+        for o in operators:
+            command += str(instances[o]) + ' '
+
+        print('Executing command ' + command)
+
+        os.system(command)
+        time.sleep(duration + 60)
+        print('Killing topology')
+        os.system('/home/vincenzo/storm/apache-storm-0.9.5/bin/storm kill ' + exp_id)
+        time.sleep(10)
+    return
+
+
+def find_most_expensive_op(stats_folder, jar, main, id_prefix, duration, repetitions, operators, instances):
+    run_exp(stats_folder, jar, main, id_prefix, duration, repetitions, operators, instances)
+
+    [throughput, latency, cost, invocations] = \
+        analyze_topology_results.analyze_topology_results(operators, instances, duration, repetitions, stats_folder,
+                                                          id_prefix)
+
+    highest_cost_op = operators[cost.index(max(cost))]
+    print('Operator with highest cost is ' + highest_cost_op)
+    instances[highest_cost_op] += 1
+    return instances
+
 
 stats_folder = '/home/vincenzo/storm_experiments/understanding_storm/results/'
 jar = '/home/vincenzo/Viper/target/Viper-0.0.1-SNAPSHOT-jar-with-dependencies.jar'
 main = 'usecases.debs2015.MergerTestNonDeterministic'
 id_prefix = 'mtndiq'
-duration = 120
+duration = 60
 repetitions = 1
 
 operators = ['spout', 'op', 'sink']
@@ -12,33 +51,9 @@ instances = {'spout': 1, 'op': 1, 'sink': 1}
 
 available_threads = 10
 
-for r in range(0, repetitions):
-
-    id = str(r) + '_'
-    for o in operators:
-        id += str(instances[o]) + '_'
-    id += id_prefix
-
-    command = '/home/vincenzo/storm/apache-storm-0.9.5/bin/storm jar ' + jar + ' ' + main + ' false true ' + \
-              stats_folder + ' ' + id + ' ' + str(duration) + ' '
-    for o in operators:
-        command += str(instances[o]) + ' '
-
-    print('Executing command ' + command)
-
-    # os.system(command)
-    # time.sleep(duration + 60)
-    # print('Killing topology')
-    # os.system('/home/vincenzo/storm/apache-storm-0.9.5/bin/storm kill ' + id)
-    # time.sleep(10)
-
-[throughput, latency, cost, invocations] = \
-    analyze_topology_results.analyze_topology_results(operators, instances, duration, repetitions, stats_folder, id_prefix)
-
-highest_cost_op = operators[cost.index(max(cost))]
-print('Operator with highest cost is ' + highest_cost_op)
-instances[highest_cost_op] += 1
+instances = find_most_expensive_op(stats_folder, jar, main, id_prefix, duration, repetitions, operators, instances)
 available_threads -= 1
 
-
-# for it in range(0, available_threads)
+while available_threads > 0:
+    instances = find_most_expensive_op(stats_folder, jar, main, id_prefix, duration, repetitions, operators, instances)
+    available_threads -= 1
