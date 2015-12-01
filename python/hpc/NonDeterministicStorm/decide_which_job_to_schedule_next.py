e000221035c30aabed4044abf686c24f09474509
Vincenzo Massimiliano Gulisano <vincenzo.gulisano@chalmers.se>
2015-12-01 09:00:53 +0100

    Updated automated experiments scripts

--- python/hpc/NonDeterministicStorm/decide_which_job_to_schedule_next.py
+++ python/hpc/NonDeterministicStorm/decide_which_job_to_schedule_next.py
@@ -1,6 +1,7 @@
 __author__ = 'vinmas'
 
 import sys
+
 sys.path.append('/nas_home/goud/.local/lib64/python2.6/site-packages/')
 from create_json_for_experiment_results import read_topology_parallel_op_data_and_store_json
 import json
@@ -27,13 +28,16 @@ if options.statefolder is None:
 
 data = json.load(open(options.statefolder + 'state.json', 'r'))
 
-print(data)
+# print(data)
+
+exp_num = data['experiment_number']
+configure_next_exp_parallelim = bool(data['exp_' + exp_num + '_config_next'])
 
 read_topology_parallel_op_data_and_store_json(
-    options.resultsfolder + data['exp_id'] + '_',
+    options.resultsfolder + data['exp_' + exp_num + '_exp_id'] + '_',
     ['spout', 'op', 'sink'],
-    [int(data['spout_parallelism']), int(data['op_parallelism']), int(data['sink_parallelism'])],
-    options.resultsfolder + 'exp_result.json')
+    [int(data['exp_' + exp_num + '_spout_parallelism']), int(data['exp_' + exp_num + '_op_parallelism']),
+     int(data['exp_' + exp_num + '_sink_parallelism'])], options.resultsfolder + 'exp_result.json')
 
 results_json = json.load(open(options.resultsfolder + 'exp_result.json', 'r'))
 
@@ -55,7 +59,8 @@ for o in operators:
     else:
         latency.append(0.0)
     cost.append(scipystat.trim_mean(results_json[o + "_cost_value"][start_ts:end_ts], 0.05) * scipystat.trim_mean(
-        results_json[o + "_invocations_value"][start_ts:end_ts], 0.05) / int(data[o + '_parallelism']) / pow(10, 9))
+        results_json[o + "_invocations_value"][start_ts:end_ts], 0.05) / int(
+        data['exp_' + exp_num + '_' + o + '_parallelism']) / pow(10, 9))
 
     print(o + ': T ' + str(throughput[-1]) + ' L ' + str(latency[-1])
           + ' C ' + str(cost[-1]))
@@ -79,37 +84,51 @@ if operators_above_threshold > 0:
     print('The thread goes to ' + highest_cost_op)
 
 # Keep some stats...
-data[data['experiment_number'] + '_spout_throughput'] = str(throughput[0])
-data[data['experiment_number'] + '_spout_latency'] = str(latency[0])
-data[data['experiment_number'] + '_spout_cost'] = str(cost[0])
-data[data['experiment_number'] + '_op_throughput'] = str(throughput[1])
-data[data['experiment_number'] + '_op_latency'] = str(latency[1])
-data[data['experiment_number'] + '_op_cost'] = str(cost[1])
-data[data['experiment_number'] + '_sink_throughput'] = str(throughput[2])
-data[data['experiment_number'] + '_sink_latency'] = str(latency[2])
-data[data['experiment_number'] + '_sink_cost'] = str(cost[2])
-data[data['experiment_number'] + '_highest_cost_op'] = highest_cost_op
-data[data['experiment_number'] + '_exp_id'] = data['exp_id']
-data[data['experiment_number'] + '_command'] = data['command']
-data[data['experiment_number'] + '_results_folder'] = options.resultsfolder
+data['exp_' + exp_num + '_spout_throughput'] = str(throughput[0])
+data['exp_' + exp_num + '_spout_latency'] = str(latency[0])
+data['exp_' + exp_num + '_spout_cost'] = str(cost[0])
+data['exp_' + exp_num + '_op_throughput'] = str(throughput[1])
+data['exp_' + exp_num + '_op_latency'] = str(latency[1])
+data['exp_' + exp_num + '_op_cost'] = str(cost[1])
+data['exp_' + exp_num + '_sink_throughput'] = str(throughput[2])
+data['exp_' + exp_num + '_sink_latency'] = str(latency[2])
+data['exp_' + exp_num + '_sink_cost'] = str(cost[2])
+data['exp_' + exp_num + '_highest_cost_op'] = highest_cost_op
+# data[data['experiment_number'] + '_exp_id'] = data['exp_id']
+# data[data['experiment_number'] + '_command'] = data['command']
+data['exp_' + exp_num + '_results_folder'] = options.resultsfolder
 
 print('\n\n')
 
-if int(data['available_threads']) > 0:
-    data['available_threads'] = str(int(data['available_threads']) - 1)
-    data['experiment_number'] = str(int(data['experiment_number']) + 1)
-    data[highest_cost_op + '_parallelism'] = str(int(data[highest_cost_op + '_parallelism']) + 1)
+prev_exp_num = exp_num
+exp_num = str(int(exp_num) + 1)
+data['experiment_number'] = exp_num
 
-    exp_id = data['repetition'] + '_' + data['spout_parallelism'] + '_' + data['op_parallelism'] + '_' + data[
-        'sink_parallelism'] + '_' + data['selectivity'] + '_' + data['load'] + '_NonDeterministicStorm'
-    exp_id = exp_id.replace('.', '-')
+if str('exp_'+exp_num+'_config_next') in data.keys():
+    if configure_next_exp_parallelim:
+        # if int(data['available_threads']) > 0:
+        #     data['available_threads'] = str(int(data['available_threads']) - 1)
+        #     data['experiment_number'] = str(int(data['experiment_number']) + 1)
+        # data[highest_cost_op + '_parallelism'] = str(int(data[highest_cost_op + '_parallelism']) + 1)
 
-    command = 'usecases.debs2015.MergerTestNonDeterministic false true \$LOGDIR \$kill_id ' + str(
-        data['duration']) + ' ' + str(data['spout_parallelism']) + ' ' + str(data['op_parallelism']) + ' ' + str(
-        data['sink_parallelism']) + ' ' + str(data['selectivity']) + ' ' + str(data['load']) + ' 1'
+        # Update parallelism
+        data['exp_' + exp_num + '_' + highest_cost_op + '_parallelism'] = str(
+            int(data['exp_' + prev_exp_num + '_' + highest_cost_op + '_parallelism']) + 1)
 
-    data['exp_id'] = exp_id
-    data['command'] = command
+        # Update exp_id and command
+        exp_id = data['exp_' + exp_num + '_rep'] + '_' + data['exp_' + exp_num + '_spout_parallelism'] + '_' + data[
+            'exp_' + exp_num + '_op_parallelism'] + '_' + data['exp_' + exp_num + '_sink_parallelism'] + '_' + data[
+                     'exp_' + exp_num + '_selectivity'] + '_' + data['exp_' + exp_num + '_load'] + '_NonDeterministicStorm'
+        exp_id = exp_id.replace('.', '-')
+
+        command = 'usecases.debs2015.MergerTestNonDeterministic false true \$LOGDIR \$kill_id ' + str(
+            data['duration']) + ' ' + str(data['exp_' + exp_num + '_spout_parallelism']) + ' ' + str(
+            data['exp_' + exp_num + '_op_parallelism']) + ' ' + str(
+            data['exp_' + exp_num + '_sink_parallelism']) + ' ' + str(
+            data['exp_' + exp_num + '_selectivity']) + ' ' + str(data['exp_' + exp_num + '_load']) + ' 1'
+
+        data['exp_' + exp_num + '_id'] = exp_id
+        data['exp_' + exp_num + '_command'] = command
 
     json.dump(data, open(options.statefolder + '/state.json', 'w'))
     create_script_and_schedule_job(data['scriptsfolder'], data['header'], data['body'],
--- python/hpc/NonDeterministicStorm/experiment_starter.py
+++ python/hpc/NonDeterministicStorm/experiment_starter.py
@@ -1,7 +1,7 @@
 __author__ = 'vinmas'
 
 from optparse import OptionParser
-#from NonDeterministicStorm.create_script_and_schedule_job import create_script_and_schedule_job
+# from NonDeterministicStorm.create_script_and_schedule_job import create_script_and_schedule_job
 from create_script_and_schedule_job import create_script_and_schedule_job
 import json
 
@@ -49,40 +49,74 @@ if options.statefolder is None:
     exit(-1)
 
 data = dict()
+
+exp_num = 1
+for load in [1.0, 0.1, 0]:
+    for selectivity in [1.0, 0.1, 0.01]:
+        for thread in range(0, 3):
+            for repetition in range(0, 1):
+                # data['exp_' + str(exp_num) + '_num'] = str(exp_num)
+                data['exp_' + str(exp_num) + '_spout_parallelism'] = "1"
+                data['exp_' + str(exp_num) + '_op_parallelism'] = "1"
+                data['exp_' + str(exp_num) + '_sink_parallelism'] = "1"
+                data['exp_' + str(exp_num) + '_load'] = str(load)
+                data['exp_' + str(exp_num) + '_selectivity'] = str(selectivity)
+                data['exp_' + str(exp_num) + '_rep'] = str(repetition)
+                data['exp_' + str(exp_num) + '_config_next'] = "True"
+                exp_num += 1
+        for repetition in range(0, 1):
+            # data['exp_' + str(exp_num) + '_num'] = str(exp_num)
+            data['exp_' + str(exp_num) + '_spout_parallelism'] = "1"
+            data['exp_' + str(exp_num) + '_op_parallelism'] = "1"
+            data['exp_' + str(exp_num) + '_sink_parallelism'] = "1"
+            data['exp_' + str(exp_num) + '_load'] = str(load)
+            data['exp_' + str(exp_num) + '_selectivity'] = str(selectivity)
+            data['exp_' + str(exp_num) + '_rep'] = str(repetition)
+            data['exp_' + str(exp_num) + '_config_next'] = "False"
+            exp_num += 1
+
 data['experiment_number'] = "1"
-data['repetition'] = "1"
-data['repetitions_per_experiment'] = "1"
+# data['repetition'] = "1"
+# data['repetitions_per_experiment'] = "1"
 data['duration'] = "300"
-data['spout_parallelism'] = "1"
-data['op_parallelism'] = "1"
-data['sink_parallelism'] = "1"
-data['available_threads'] = "10"
-data['assigned_threads'] = "0"
-data['max_selectivity'] = "1.0"
-data['min_selectivity'] = "0.1"
-data['selectivity_step'] = "0.1"
-data['selectivity'] = "1.0"
-data['max_load'] = "1.0"
-data['min_load'] = "0.0"
-data['load_step'] = "0.1"
-data['load'] = "1.0"
+# data['spout_parallelism'] = "1"
+# data['op_parallelism'] = "1"
+# data['sink_parallelism'] = "1"
+# data['available_threads'] = "10"
+# data['assigned_threads'] = "0"
+# data['max_selectivity'] = "1.0"
+# data['min_selectivity'] = "0.1"
+# data['selectivity_step'] = "0.1"
+# data['selectivity'] = "1.0"
+# data['max_load'] = "1.0"
+# data['min_load'] = "0.0"
+# data['load_step'] = "0.1"
+# data['load'] = "1.0"
 data['scriptsfolder'] = options.scriptsfolder
 data['header'] = options.header
 data['body'] = options.body
 data['script'] = options.script
 data['runner'] = options.runner
 
-exp_id = data['repetition'] + '_' + data['spout_parallelism'] + '_' + data['op_parallelism'] + '_' + data[
-    'sink_parallelism'] + '_' + data['selectivity'] + '_' + data['load'] + '_NonDeterministicStorm'
+exp_id = data['exp_' + data['experiment_number'] + '_rep'] + '_' + data[
+    'exp_' + data['experiment_number'] + '_spout_parallelism'] + '_' + data[
+             'exp_' + data['experiment_number'] + '_op_parallelism'] + '_' + data[
+             'exp_' + data['experiment_number'] + '_sink_parallelism'] + '_' + data[
+             'exp_' + data['experiment_number'] + '_selectivity'] + '_' + data[
+             'exp_' + data['experiment_number'] + '_load'] + '_NonDeterministicStorm'
 exp_id = exp_id.replace('.', '-')
 
 command = 'usecases.debs2015.MergerTestNonDeterministic false true \$LOGDIR \$kill_id ' + str(
-    data['duration']) + ' ' + str(data['spout_parallelism']) + ' ' + str(data['op_parallelism']) + ' ' + str(
-    data['sink_parallelism']) + ' ' + str(data['selectivity']) + ' ' + str(data['load']) + ' 1'
+    data['duration']) + ' ' + str(
+    data['exp_' + data['experiment_number'] + '_spout_parallelism']) + ' ' + str(
+    data['exp_' + data['experiment_number'] + '_op_parallelism']) + ' ' + str(
+    data['exp_' + data['experiment_number'] + '_sink_parallelism']) + ' ' + str(
+    data['exp_' + data['experiment_number'] + '_selectivity']) + ' ' + str(
+    data['exp_' + data['experiment_number'] + '_load']) + ' 1'
 
-data['exp_id'] = exp_id
-data['command'] = command
+data['exp_' + data['experiment_number'] + '_id'] = exp_id
+data['exp_' + data['experiment_number'] + '_command'] = command
 
 json.dump(data, open(options.statefolder + '/state.json', 'w'))
 create_script_and_schedule_job(options.scriptsfolder, options.header, options.body,
-                                                          options.script, exp_id, command, options.runner)
+                               options.script, exp_id, command, options.runner)
